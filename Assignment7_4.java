38d1e2993e576706c4fb257a94c7b557770a5e73
truongbao baotruong <nguyentruongbao12a1@gmail.com>
2024-11-09 22:06:57 +0700

    Assignment7_4.java

--- Assignment7_4.java
+++ Assignment7_4.java
@@ -2,7 +2,15 @@ import java.util.ArrayList;
 
 public class Assignment7_4 {
     public static void main(String[] args) {
-        ArrayList<String> a = new ArrayList<>("Java", "Python", "Javascript", "C++", "Php");
-
+        ArrayList<String> a = new ArrayList<>();
+        a.add("Java");
+        a.add("Python");
+        a.add("C++");
+        a.add("JavaScript");
+        if (a.contains("Python")) {
+            System.out.println("Python is in the list.");
+        } else {
+            System.out.println("Python is not in the list.");
+        }
     }
 }
